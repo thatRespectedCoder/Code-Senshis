c0904db632d25bdd4d2b60f61a070a9359ee0ab4
thatRespectedCoder <shreyasms700@gmail.com>
2024-02-10 20:37:24 +0530

    cache commit

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const eventRoutes = require('./routes/eventRoutes');
 const userRoutes = require('./routes/userRoutes');
-require('dotenv').config(); // Load environment variables from .env file
+const NodeCache = require('node-cache');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/event_management_system';
+const MONGODB_URI = process.env.MONGODB_URI ;
 
 // MongoDB connection
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+    // Clear cache when major changes occur in the database
+    db.on('major-change', () => {
+        cache.flushAll(); // Flush all cached data
+        console.log('Cache flushed due to major change in database');
+    });
+});
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
